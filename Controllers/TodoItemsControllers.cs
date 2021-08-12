1b5017e92581e7357899bf5d4830e5466b93bc03
Your Name <you@example.com>
2021-08-12 13:10:58 -0300

    Método Alterar feito(Update)

--- Controllers/TodoItemsControllers.cs
+++ Controllers/TodoItemsControllers.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using senai.todolist.webapi.Models;
+using System;
+
 
 namespace senai.todolist.webapi.Controllers
 {
@@ -26,6 +28,24 @@ namespace senai.todolist.webapi.Controllers
             if (todoitems.Count == 0)
                 return NoContent();
 
+            return Ok(todoitems);
+        }
+
+        //Atualiza a tarefa/item
+        [HttpPut("{id}")] //https//localhost:5001/api/todoitems/id
+        public IActionResult Put(Guid id, TodoItem todoItem){
+
+            //busca o indice da lista referente a tarefa
+            int todoIndex = todoitems.FindIndex(t => t.Id == id);
+
+            //verifica se a tarefa foi encontrada
+            if(todoIndex == -1)
+                    return BadRequest(new {mensage = "Id não encontrado."});
+
+            //atualiza a tarefa
+            todoitems[todoIndex] = todoItem;
+
+            //retorna sucesso com a lista de tarefas atualizada
             return Ok(todoitems);
         }
     }
