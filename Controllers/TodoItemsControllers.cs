68104d7a6e2aebd934d7bf70f6cc22d2ca8f3122
Your Name <you@example.com>
2021-08-12 13:22:35 -0300

    Método Deletar e Ler por Id feitos(Delete and GetById)

--- Controllers/TodoItemsControllers.cs
+++ Controllers/TodoItemsControllers.cs
@@ -13,21 +13,28 @@ namespace senai.todolist.webapi.Controllers
     {
         private static List<TodoItem> todoitems = new List<TodoItem>();
 
+        //cadatra uma nova tarefa
         [HttpPost]
         public IActionResult Post(TodoItem todoItem)
         {
-
+            //salva a tarefa na lista de tarefas
             todoitems.Add(todoItem);
 
+            //retorna sucesso com a tarefa que foi cadastrada
             return Ok(todoItem);
         }
 
+        //lista todas as tarefas
         [HttpGet]
         public IActionResult Get()
         {
+
+            //verifica se existe tarefas na lista
+            //caso nao exista retorna erro 204/erro de NoContent
             if (todoitems.Count == 0)
                 return NoContent();
 
+            //caso exista retorna sucesso com a lista de tarefas
             return Ok(todoitems);
         }
 
@@ -47,6 +54,35 @@ namespace senai.todolist.webapi.Controllers
 
             //retorna sucesso com a lista de tarefas atualizada
             return Ok(todoitems);
+        }
+
+        [HttpDelete("{id}")] //id do objeto q eu quero deletar
+        public IActionResult Delete(Guid id){
+            //busca o indice da lista referente a tarefa
+            int todoIndex = todoitems.FindIndex(t => t.Id == id);
+
+            //verifica se a tarefa foi encontrada
+            if(todoIndex == -1)
+                    return BadRequest(new {mensage = "Id não encontrado."});
+
+            //remove a tarefa da lista de tarefas
+            todoitems.Remove(todoitems[todoIndex]);
+
+            //retorna sucesso com a lista de tarefas
+            return Ok(todoitems);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id){
+            //busca o indice da lista referente a tarefa
+            int todoIndex = todoitems.FindIndex(t => t.Id == id);
+
+            //verifica se a tarefa foi encontrada
+            if(todoIndex == -1)
+                    return BadRequest(new {mensage = "Id não encontrado."});
+
+            //retorna sucesso com a tarefa buscada
+            return Ok(todoitems[todoIndex]);
         }
     }
 }
